6cf4ba84cbb783d1b4ad40d69e22e72825f71f65
Salil Upadhyay <upsalil.01@gmail.com>
2023-05-03 15:36:20 +0530

    added authntication on user routes

--- backend/routes/User.route.js
+++ backend/routes/User.route.js
@@ -1,31 +1,42 @@
 const express = require("express");
 const { UserModel } = require("../models/User.model");
 const bcrypt = require("bcrypt");
+const { auth } = require("../middlewares/auth.middleware");
 const userRouter = express.Router();
 
 /* ------ Get all User ------ */
-userRouter.get("/", async (req, res) => {
-  try {
-    const data = await UserModel.find();
-    res.status(200).send({ users: data });
-  } catch (error) {
-    res.status(400).send(error);
+//only admin can access
+userRouter.get("/", auth, async (req, res) => {
+  let data = req.body;
+  if (data.role === "admin") {
+    try {
+      const data = await UserModel.find();
+      res.status(200).send({ users: data });
+    } catch (error) {
+      res.status(400).send(error);
+    }
+  } else {
+    res.status(200).send({ msg: "You are not Authorized" });
   }
 });
 // /* ------ Get one  User ------ */
-// userRouter.get("/:id", async (req, res) => {
-//   const { id } = req.params;
-//   try {
-//     let user = await UserModel.find({ _id: id });
-//     res.status(200).send({ user: user[0] });
-//   } catch (error) {}
-// });
+
+userRouter.get("/:id", auth, async (req, res) => {
+  const data = req.body;
+  const { id } = req.params;
+  const user = await UserModel.findOne({ _id: id });
+  if (data.role === "admin" || data.authorID == user._id) {
+    res.status(200).send({ user: user });
+  } else {
+    res.status(200).send({ msg: "You are not Authorized" });
+  }
+});
 
 /* ------ Register User ------ */
 userRouter.post("/register", async (req, res) => {
   //logic
   const { email, password, number, name } = req.body;
-  
+
   try {
     //hashing password
     bcrypt.hash(password, 5, async (err, hash) => {
