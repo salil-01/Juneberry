21067006625200fda05b8ecd7b62b19e32b82728
Vikas23sharma <112754659+Vikas23sharma@users.noreply.github.com>
2023-05-04 22:13:09 +0530

    product page static UI

--- frontend/src/pages/ProductPage.jsx
+++ frontend/src/pages/ProductPage.jsx
@@ -1,6 +1,67 @@
-export const ProductPage = ()=>{
+import { Box, Button, Flex, Heading, SimpleGrid } from "@chakra-ui/react"
+import "../styles/productpage.css"
+import Productsidebar from "../components/Productsidebar"
+import { useDispatch, useSelector } from "react-redux"
+import { useEffect } from "react"
+import { getMenProduct } from "../redux/productReducer/action"
+
+export const ProductPage = () => {
+    const dispatch = useDispatch()
+    // const
+    useEffect(() => {
+        let obj = {
+            params: {
+                // brand: "pilcro",
+            },
+        };
+        dispatch(getMenProduct(obj))
+    }, [])
+    const { products } = useSelector((store) => store.DressReducer)
+    console.log(products)
     return (
         <>
-        Product Page</>
+            <SimpleGrid className="topgrid" >
+                <Box className="gridbox" bg='#b18341' height='80px'>DRESSES</Box>
+                <Box className="gridbox" bg='#9a5462' height='80px'>PANTS</Box>
+                <Box className="gridbox" bg='#b48779' height='80px'>JEANS</Box>
+                <Box className="gridbox" bg='#cc835a' height='80px'>GETAWAY & RESORT <br />WEAR</Box>
+                <Box className="gridbox" bg='#887277' height='80px'>TOPS</Box>
+                <Box className="gridbox" bg='#a57772' height='80px'>INTIMATES</Box>
+            </SimpleGrid>
+            <Box className="maincontainer">
+                <Box className="sidebarcontainer">
+                    <Heading size={"md"} >Filter By:</Heading>
+                    <Productsidebar />
+                </Box>
+                <Box className="productscontainer">
+                    <Flex className="productscontainertop">
+                        <Box className="label"><Heading size={"md"} >Dresses</Heading></Box>
+                        <Flex className="sortcontainer" >
+                            <select>
+                                <option>Sort By</option>
+                                <option>High to low</option>
+                                <option>Low to High</option>
+                            </select>
+                            <Box>
+                                <Button>PREV</Button>
+                                <span>1</span>
+                                <Button>NEXT</Button>
+
+                            </Box>
+                        </Flex>
+                    </Flex>
+                    <Box className="productscontainer2">
+                    {products.map((el) => {
+                        return <Box style={{ color: "gray", textAlign: "left", padding: "3%" }}>
+                            <img src={el.img} alt={el.name} />
+                            <p>{el.name}</p>
+                            <p>${el.mrp}</p>
+                            🟢🔵(2colors)
+                        </Box>
+                    })}
+                    </Box>
+                </Box>
+            </Box>
+        </>
     )
 }
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,4 +1,5 @@
 import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
+import { reducer as DressReducer } from "./productReducer/reducer";
 import thunk from "redux-thunk";
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({ DressReducer });
 export const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
