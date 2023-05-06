1216bd18c51f3b8452a227e2162a9f16686be57c
Vikas23sharma <112754659+Vikas23sharma@users.noreply.github.com>
2023-05-06 21:15:18 +0530

    product page functionality

--- frontend/src/components/Productsidebar.jsx
+++ frontend/src/components/Productsidebar.jsx
@@ -1,6 +1,7 @@
 import { Box, Flex, Text } from '@chakra-ui/react'
 import "../styles/productpage.css"
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { AddIcon } from '@chakra-ui/icons'
 import {
     Accordion,
     AccordionItem,
@@ -8,107 +9,149 @@ import {
     AccordionPanel,
     AccordionIcon,
 } from '@chakra-ui/react'
+import { useSearchParams } from 'react-router-dom'
+
+
 
 const Productsidebar = () => {
+    const [searchParams, setSearchParams] = useSearchParams()
+    const initialbrand = searchParams.getAll("brand")
+    const [brand, setBrand] = useState(initialbrand || [])
+
+    const handleChange = (e) => {
+        const value = e.target.value
+        let newBrand = [...brand]
+
+        if (newBrand.includes(value)) {
+            newBrand = newBrand.filter((el) => el !== value)
+        } else {
+            newBrand.push(value)
+        }
+        setBrand(newBrand)
+    }
+
+    useEffect(() => {
+        const params = {
+            brand
+        }
+        setSearchParams(params)
+    }, [brand])
     return (
         <Flex className='sidebar'>
-            
-                <Accordion defaultIndex={[0]} allowMultiple>
-                    <AccordionItem >
-                        <h2>
-                            <AccordionButton>
-                                <Box as="span" flex='1' textAlign='center'>
-                                    BRAND
-                                </Box>
-                                <AccordionIcon />
-                            </AccordionButton>
-                        </h2>
-                        <AccordionPanel pb={4}>
-                            <Text padding={"1%"} boxShadow={"rgba(99, 99, 99, 0.2) 0px 2px 8px 0px"} className='sidebaroptions'>Maeve</Text>
-                            <Text padding={"1%"} boxShadow={"rgba(99, 99, 99, 0.2) 0px 2px 8px 0px"} className='sidebaroptions'>Sloggi</Text>
-                            <Text padding={"1%"} boxShadow={"rgba(99, 99, 99, 0.2) 0px 2px 8px 0px"} className='sidebaroptions'>Pilcro</Text>
-                        </AccordionPanel>
-                    </AccordionItem>
-                </Accordion>
-           
-            
-                <Accordion defaultIndex={[0]} allowMultiple>
-                    <AccordionItem >
-                        <h2>
-                            <AccordionButton>
-                                <Box as="span" flex='1' textAlign='center'>
-                                    COLOR
-                                </Box>
-                                <AccordionIcon />
-                            </AccordionButton>
-                        </h2>
-                        <AccordionPanel pb={4}>
-                            <Text padding={"1%"} boxShadow={"rgba(99, 99, 99, 0.2) 0px 2px 8px 0px"} className='sidebaroptions'>White ⚪</Text>
-                            <Text padding={"1%"} boxShadow={"rgba(99, 99, 99, 0.2) 0px 2px 8px 0px"} className='sidebaroptions'>Blue  🔵</Text>
-                            <Text padding={"1%"} boxShadow={"rgba(99, 99, 99, 0.2) 0px 2px 8px 0px"} className='sidebaroptions'>Red   🔴</Text>
-                            <Text padding={"1%"} boxShadow={"rgba(99, 99, 99, 0.2) 0px 2px 8px 0px"} className='sidebaroptions'>Green 🟢</Text>
-                        </AccordionPanel>
-                    </AccordionItem>
-                </Accordion>
-            
-           
-                <Accordion defaultIndex={[0]} allowMultiple>
-                    <AccordionItem>
-                        <h2>
-                            <AccordionButton>
-                                <Box as="span" flex='1' textAlign='center'>
-                                    SIZE
-                                </Box>
-                                <AccordionIcon />
-                            </AccordionButton>
-                        </h2>
-                        <AccordionPanel pb={4}>
-                            <Text padding={"1%"} boxShadow={"rgba(99, 99, 99, 0.2) 0px 2px 8px 0px"} className='sidebaroptions'>Small</Text>
-                            <Text padding={"1%"} boxShadow={"rgba(99, 99, 99, 0.2) 0px 2px 8px 0px"} className='sidebaroptions'>Medium</Text>
-                            <Text padding={"1%"} boxShadow={"rgba(99, 99, 99, 0.2) 0px 2px 8px 0px"} className='sidebaroptions'>Large</Text>
-                            <Text padding={"1%"} boxShadow={"rgba(99, 99, 99, 0.2) 0px 2px 8px 0px"} className='sidebaroptions'>X-Large</Text>
-                        </AccordionPanel>
-                    </AccordionItem>
-                </Accordion>
-            
-                <Accordion defaultIndex={[0]} allowMultiple>
-                    <AccordionItem>
-                        <h2>
-                            <AccordionButton>
-                                <Box as="span" flex='1' textAlign='center'>
-                                    PRODUCT TYPE
-                                </Box>
-                                <AccordionIcon />
-                            </AccordionButton>
-                        </h2>
-                        <AccordionPanel pb={4}>
-                            <Text padding={"1%"} boxShadow={"rgba(99, 99, 99, 0.2) 0px 2px 8px 0px"} className='sidebaroptions'>Tops</Text>
-                            <Text padding={"1%"} boxShadow={"rgba(99, 99, 99, 0.2) 0px 2px 8px 0px"} className='sidebaroptions'>Dresses</Text>
-                            <Text padding={"1%"} boxShadow={"rgba(99, 99, 99, 0.2) 0px 2px 8px 0px"} className='sidebaroptions'>Pants</Text>
-                            <Text padding={"1%"} boxShadow={"rgba(99, 99, 99, 0.2) 0px 2px 8px 0px"} className='sidebaroptions'>Jeans</Text>
-                        </AccordionPanel>
-                    </AccordionItem>
-                </Accordion>
-            
-                <Accordion defaultIndex={[0]} allowMultiple>
-                    <AccordionItem>
-                        <h2>
-                            <AccordionButton>
-                                <Box as="span" flex='1' textAlign='center'>
-                                    CATEGORY
-                                </Box>
-                                <AccordionIcon />
-                            </AccordionButton>
-                        </h2>
-                        <AccordionPanel pb={4}>
-                            <Text padding={"1%"} boxShadow={"rgba(99, 99, 99, 0.2) 0px 2px 8px 0px"} className='sidebaroptions'>New!!</Text>
-                            <Text padding={"1%"} boxShadow={"rgba(99, 99, 99, 0.2) 0px 2px 8px 0px"} className='sidebaroptions'>Top-Rated</Text>
-                            <Text padding={"1%"} boxShadow={"rgba(99, 99, 99, 0.2) 0px 2px 8px 0px"} className='sidebaroptions'>Activewear</Text>
-                            <Text padding={"1%"} boxShadow={"rgba(99, 99, 99, 0.2) 0px 2px 8px 0px"} className='sidebaroptions'>Blazers</Text>
-                        </AccordionPanel>
-                    </AccordionItem>
-                </Accordion>
-         
+
+            <Accordion defaultIndex={[0]} allowMultiple>
+                <AccordionItem >
+                    <h2>
+                        <AccordionButton>
+                            <Box className='sidebarfilters' as="span" flex='1' textAlign='Left'>
+                                Brand
+                            </Box>
+                            <AddIcon />
+                        </AccordionButton>
+                    </h2>
+                    <AccordionPanel textAlign='Left' pb={4}>
+                        <Text padding={"1%"} className='sidebaroptions'>
+                            <input type='checkbox' value="Maeve" onChange={handleChange}
+                                checked={brand.includes("Maeve")} /> Maeve
+                        </Text>
+                        <Text padding={"1%"} className='sidebaroptions'>
+                            <input type='checkbox' value="Sloggi" onChange={handleChange}
+                                checked={brand.includes("Sloggi")} /> Sloggi
+                        </Text>
+                        <Text padding={"1%"} className='sidebaroptions'>
+                            <input type='checkbox' value="Pilcro" onChange={handleChange}
+                                checked={brand.includes("Pilcro")} /> Pilcro
+                        </Text>
+                    </AccordionPanel>
+                </AccordionItem>
+            </Accordion>
+
+
+            <Accordion defaultIndex={[0]} allowMultiple>
+                <AccordionItem >
+                    <h2>
+                        <AccordionButton>
+                            <Box className='sidebarfilters' as="span" flex='1' textAlign='Left'>
+                                Color
+                            </Box>
+                            <AddIcon />
+                        </AccordionButton>
+                    </h2>
+                    <AccordionPanel pb={4}>
+                        <Text padding={"1%"} className='sidebaroptions'>
+                            <input type='checkbox' /> White ⚪
+                        </Text>
+                        <Text padding={"1%"} className='sidebaroptions'>
+                            <input type='checkbox' /> Blue  🔵
+                        </Text>
+                        <Text padding={"1%"} className='sidebaroptions'>
+                            <input type='checkbox' /> Red   🔴</Text>
+                        <Text padding={"1%"} className='sidebaroptions'>
+                            <input type='checkbox' /> Green 🟢
+                        </Text>
+                    </AccordionPanel>
+                </AccordionItem>
+            </Accordion>
+
+
+            <Accordion defaultIndex={[0]} allowMultiple>
+                <AccordionItem>
+                    <h2>
+                        <AccordionButton>
+                            <Box className='sidebarfilters' as="span" flex='1' textAlign='Left'>
+                                Size
+                            </Box>
+                            <AddIcon />
+                        </AccordionButton>
+                    </h2>
+                    <AccordionPanel pb={4}>
+                        <Text padding={"1%"} className='sidebaroptions'>
+                            <input type='checkbox' /> Small</Text>
+                        <Text padding={"1%"} className='sidebaroptions'><input type='checkbox' /> Medium</Text>
+                        <Text padding={"1%"} className='sidebaroptions'><input type='checkbox' /> Large</Text>
+                        <Text padding={"1%"} className='sidebaroptions'> <input type='checkbox' /> X-Large</Text>
+                    </AccordionPanel>
+                </AccordionItem>
+            </Accordion>
+
+            <Accordion defaultIndex={[0]} allowMultiple>
+                <AccordionItem>
+                    <h2>
+                        <AccordionButton>
+                            <Box className='sidebarfilters' as="span" flex='1' textAlign='Left'>
+                                Product Type
+                            </Box>
+                            <AddIcon />
+                        </AccordionButton>
+                    </h2>
+                    <AccordionPanel pb={4}>
+                        <Text padding={"1%"} className='sidebaroptions'><input type='checkbox' /> Tops</Text>
+                        <Text padding={"1%"} className='sidebaroptions'><input type='checkbox' /> Dresses</Text>
+                        <Text padding={"1%"} className='sidebaroptions'><input type='checkbox' /> Pants</Text>
+                        <Text padding={"1%"} className='sidebaroptions'><input type='checkbox' /> Jeans</Text>
+                    </AccordionPanel>
+                </AccordionItem>
+            </Accordion>
+
+            <Accordion defaultIndex={[0]} allowMultiple>
+                <AccordionItem>
+                    <h2>
+                        <AccordionButton>
+                            <Box className='sidebarfilters' as="span" flex='1' textAlign='Left'>
+                                Category
+                            </Box>
+                            <AddIcon />
+                        </AccordionButton>
+                    </h2>
+                    <AccordionPanel pb={4}>
+                        <Text padding={"1%"} className='sidebaroptions'><input type='checkbox' /> New!!</Text>
+                        <Text padding={"1%"} className='sidebaroptions'><input type='checkbox' /> Top-Rated</Text>
+                        <Text padding={"1%"} className='sidebaroptions'><input type='checkbox' /> Activewear</Text>
+                        <Text padding={"1%"} className='sidebaroptions'><input type='checkbox' /> Blazers</Text>
+                    </AccordionPanel>
+                </AccordionItem>
+            </Accordion>
+
         </Flex>
     )
 }
--- frontend/src/pages/ProductPage.jsx
+++ frontend/src/pages/ProductPage.jsx
@@ -1,23 +1,56 @@
-import { Box, Button, Flex, Heading, SimpleGrid } from "@chakra-ui/react"
+import { Box, Button, Flex, Heading, SimpleGrid, Text } from "@chakra-ui/react"
 import "../styles/productpage.css"
 import Productsidebar from "../components/Productsidebar"
 import { useDispatch, useSelector } from "react-redux"
-import { useEffect } from "react"
-import { getMenProduct } from "../redux/productReducer/action"
+import { useEffect, useState } from "react"
+import { getDressProduct } from "../redux/productReducer/action"
+import { ChevronRightIcon, ChevronLeftIcon } from "@chakra-ui/icons"
+import { useLocation, useSearchParams } from "react-router-dom"
+
 
 export const ProductPage = () => {
+
+    const [searchParams, setSearchParams] = useSearchParams()
+    const initialsort = searchParams.get("sort")
+    const brand = searchParams.getAll("brand")
+    const [sort, setSort] = useState(initialsort || "")
+    const initialpage = searchParams.get("page")
+    const [page, setPage] = useState(initialpage || 1)
+    const location = useLocation()
     const dispatch = useDispatch()
-    // const
+
+    const handlesort = (e) => {
+        setSort(e.target.value)
+
+    }
+
+    const handlepage = (val) => {
+        setPage(page + val)
+    }
+
+
     useEffect(() => {
+        if (products.length < 9) { setPage(1) }
+        const params = {
+            brand,
+            page
+        }
+        sort && (params.sort = sort)
+        setSearchParams(params)
+
         let obj = {
             params: {
-                // brand: "pilcro",
-            },
+                brand,
+                page
+            }
         };
-        dispatch(getMenProduct(obj))
-    }, [])
+        // (!brand) && (obj.params.page=page)
+        sort && (obj.params.sort = sort)
+        dispatch(getDressProduct(obj))
+    }, [location.search, sort, page])
+
     const { products } = useSelector((store) => store.DressReducer)
-    console.log(products)
+    console.log(products.length)
     return (
         <>
             <SimpleGrid className="topgrid" >
@@ -37,28 +70,33 @@ export const ProductPage = () => {
                     <Flex className="productscontainertop">
                         <Box className="label"><Heading size={"md"} >Dresses</Heading></Box>
                         <Flex className="sortcontainer" >
-                            <select>
-                                <option>Sort By</option>
-                                <option>High to low</option>
-                                <option>Low to High</option>
+                            <select onChange={handlesort}>
+                                <option value="">Sort By</option>
+                                <option value="desc">High to low</option>
+                                <option value="asc">Low to High</option>
+                                <option value="">Newest</option>
+                                <option value="">Bestselling</option>
+                                <option value="">A to Z</option>
+                                <option value="">Z to A</option>
                             </select>
                             <Box>
-                                <Button>PREV</Button>
-                                <span>1</span>
-                                <Button>NEXT</Button>
+                                <Button variant='ghost' leftIcon={<ChevronLeftIcon boxSize={10} />} isDisabled={page === 1} onClick={() => handlepage(-1)} ></Button>
+                                {page}
+                                <Button variant='ghost' isDisabled={products.length < 9} onClick={() => handlepage(1)} leftIcon={<ChevronRightIcon boxSize={10} />}></Button>
+
 
                             </Box>
                         </Flex>
                     </Flex>
                     <Box className="productscontainer2">
-                    {products.map((el) => {
-                        return <Box style={{ color: "gray", textAlign: "left", padding: "3%" }}>
-                            <img src={el.img} alt={el.name} />
-                            <p>{el.name}</p>
-                            <p>${el.mrp}</p>
-                            🟢🔵(2colors)
-                        </Box>
-                    })}
+                        {products.map((el) => {
+                            return <Box key={el._id} style={{ color: "gray", textAlign: "left", padding: "3%" }}>
+                                <img src={el.img} alt={el.name} />
+                                <p>{el.name}</p>
+                                <span>${el.price}</span>
+                                <p>🟢🔵(2colors)</p>
+                            </Box>
+                        })}
                     </Box>
                 </Box>
             </Box>
